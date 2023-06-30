09b2c83c877ce808bfc18647ffab4d876001c381
zhanghongjian <zhanghongjian-ghq>
2023-06-30 17:32:29 +0800

    edit

--- js/functions.js
+++ js/functions.js
@@ -114,6 +114,28 @@ function timeElapse(date){
 	$("#elapseClock").html(result);
 }
 
+function timeElapse1(date){
+	var current = Date();
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var days = Math.floor(seconds / (3600 * 24))+31;
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600);
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	seconds = seconds % 3600;
+	var minutes = Math.floor(seconds / 60);
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	seconds = seconds % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
+	$("#elapseClock1").html(result);
+}
+
 function showMessages() {
 	adjustWordsPosition();
 	$('#messages').fadeIn(5000, function() {
